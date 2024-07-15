2758b0202e7350e17e815672793998f10c17a6df
Luca Bandini <152310492+lucaxbandini@users.noreply.github.com>
2024-07-15 18:00:25 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "LBJenkin" {
+    ami = "ami-04a81a99f5ec58529"
+    instance_type = "t2.micro"
+    tags = {
+        name = "LBJenkin"
+    }
+}
