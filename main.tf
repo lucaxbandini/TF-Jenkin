62b7b379d09d404224ce48c3cd7d88ab741b56a4
Luca Bandini <152310492+lucaxbandini@users.noreply.github.com>
2024-07-19 12:53:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 resource "aws_instance" "LBJenkin" {
     ami = "ami-04a81a99f5ec58529"
     instance_type = "t2.micro"
-    count = "3"
+    count = 3
     tags = {
-        name = "LBJenkin"
+        name = "LBJenkin-${count.index}"
     }
 }
